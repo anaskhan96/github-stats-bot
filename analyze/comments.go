8b1cbf93bce29ad0e872eeec4ddd52ccb4e3bba0
Anas Khan <anas.khan96@outlook.com>
2017-07-07 23:25:04 +0530

    added comments.go

---
+++ analyze/comments.go
@@ -0,0 +1,15 @@
+package analyze
+
+import (
+	"github.com/jzelinskie/geddit"
+)
+
+func FindRelevantComments(comments []*geddit.Comment) ([]*geddit.Comment, error) {
+	// TODO
+	return comments, nil
+}
+
+func PostReplies(comments []*geddit.Comment) error {
+	// TODO
+	return nil
+}
