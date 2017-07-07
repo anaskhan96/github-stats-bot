083b778a4e87535f77bddf61c8629088f585b5bb
Anas Khan <anas.khan96@outlook.com>
2017-07-07 23:06:11 +0530

    added a polling function and firing up a server

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
+
+	"net/http"
 
 	"github.com/joho/godotenv"
 	"github.com/jzelinskie/geddit"
 )
+
+// default interval for hitting the api in seconds
+const interval = 3
 
 func main() {
 	var env map[string]string
@@ -13,6 +20,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	session, err := geddit.NewOAuthSession(env["clientID"], env["clientSecret"], env["userAgent"], env["redirectURL"])
 	if err != nil {
 		log.Fatal(err)
@@ -21,5 +29,30 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(session.Me())
+
+	go startPolling(session)
+
+	// firing up a server to keep the process running
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8081", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go program running %s!", r.URL.Path[1:])
+}
+
+func startPolling(s *geddit.OAuthSession) {
+	for {
+		time.Sleep(interval * time.Second)
+		go fetchComments(s)
+	}
+}
+
+func fetchComments(s *geddit.OAuthSession) {
+	comments, err := s.SubredditComments("all")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(comments[0].Body)
 }
